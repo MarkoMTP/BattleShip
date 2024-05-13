b8f27f5e1ef0f1e155dcd5858ab8ec93945ede4a
MarkoMTP <marko.mtp8@gmail.com>
2024-05-13 17:07:03 +0200

    gameboard factory func done

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,3 +1,27 @@
-export default function Gameboard() {
+import Ship from './ship';
 
+export default function Gameboard(board = []) {
+  // Step 1: Create the Board
+  for (let row = 0; row < 10; row++) {
+    const rowArray = [];
+    for (let col = 0; col < 10; col++) {
+      const cell = {
+        row, col, object: null, hasShip: false,
+      };
+      rowArray.push(cell);
+    }
+    board.push(rowArray);
+  }
+
+  // Step 2: Return the Board
+  return {
+    board,
+
+    placeShip(a, b, lenght) {
+      const { board } = this;
+      board[a][b].hasShip = true;
+      board[a][b].object = new Ship(lenght);
+      return board[a][b];
+    },
+  };
 }
