82c66f1b6548510981540600b2e2da71da3d364e
MarkoMTP <marko.mtp8@gmail.com>
2024-05-18 11:22:07 +0200

    fixed the hit and sunk implementetion of both ship and its parts

--- src/ship.js
+++ src/ship.js
@@ -1,39 +1,38 @@
-export default function Ship(length, hitTimes = 0, isSunk = false) {
+export default function Ship(length) {
   const shipParts = [];
   for (let i = 0; i < length; i++) {
-    const newPart = new ShipPart();
+    const newPart = ShipPart();
     shipParts.push(newPart);
   }
 
   return {
     length,
-    hitTimes,
-    isSunk,
     shipParts,
 
     isItSunk() {
-      this.shipParts.forEach((part) => {
-        if (part.isSunk) {
-          this.hitTimes++;
-        }
-      });
-      if (this.hitTimes >= length) {
+      // Check if all parts are sunk
+      const allPartsSunk = this.shipParts.every((part) => part.isSunk);
+      if (allPartsSunk) {
+        this.isSunk = true;
         return true;
-      } return false;
+      }
+      return false;
     },
   };
 }
 
-export function ShipPart(hitTimes = 0, isSunk = false) {
+export function ShipPart() {
   return {
-    hitTimes,
-    isSunk,
+    hitTimes: 0,
+    isSunk: false,
 
     isHit() {
-      this.hitTimes++;
-      this.isSunk = true;
-      return isSunk;
+      if (!this.isSunk) {
+        this.hitTimes++;
+        this.isSunk = true; // Assume the part is considered sunk after one hit
+        return 'hit';
+      }
+      return 'already sunk';
     },
-
   };
 }
