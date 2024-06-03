4fbfd8d743a7103822e824a014cbdeddfe844b92
MarkoMTP <marko.mtp8@gmail.com>
2024-06-03 12:03:38 +0200

    edited the title

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import randomGame, { randomComputerOnly } from './randomGame';
 const container = document.getElementById('container');
 
 const playRandomGameBtn = document.createElement('button');
-playRandomGameBtn.classList.add('randomBtn');
+playRandomGameBtn.classList.add('randomGBtn');
 playRandomGameBtn.textContent = 'Play Game';
 playRandomGameBtn.addEventListener('click', () => {
   container.innerHTML = ''; // Clear the container
